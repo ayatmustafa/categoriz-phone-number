77a71e16f60791b7e0501dd6a9bbc22c7ec45d2e
ayatmustafa72 <ayat.mustafa@cowpay.me>
2023-01-24 15:00:57 +0200

    update helpers file

--- app/helpers.php
+++ app/helpers.php
@@ -10,6 +10,7 @@
     {
         $page = $page ?: (Illuminate\Pagination\Paginator::resolveCurrentPage() ?: 1);
         $items = $items instanceof Illuminate\Support\Collection ? $items : Illuminate\Support\Collection::make($items);
+
         return new Illuminate\Pagination\LengthAwarePaginator($items->forPage($page, $perPage), $items->count(), $perPage, $page, $options);
     }
 
